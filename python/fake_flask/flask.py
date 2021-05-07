1b3ead1cb11197a0365351b0d86744461dacfc4c
geoisevil <geoisevil@gmail.com>
2021-05-07 13:26:32 -0700

    support for type casting path variables

--- python/fake_flask/flask.py
+++ python/fake_flask/flask.py
@@ -2,18 +2,23 @@ import socket#builtin library for creating websockets
 import os
 from typing import Any, Callable, Dict, Optional, Tuple#just used for type hints, doesn't actually affect functionality
 
+#TODO maybe make this a class attribute?
+TYPES = {
+    "int" : lambda x : int(x) if x.isdigit() else None,
+    "string" : lambda x : x if not x.isdigit() else None,
+}
+
 class Flask:
     def __init__(self,import_name:str) -> None:#constructor function
         self.import_name = import_name#currently not used
         self.paths = {}#dict to keep track of registered paths
 
-    def route(self,path:str) -> Callable:#the @app.route decorator
-        def register(func:Callable) -> Callable:#basic decorator structure/closure
+    def route(self,path:str) -> Callable:#create the @app.route decorator
+        def register(func:Callable) -> Callable:#func is the function below the decorator
             self.paths[path] = func#store path as dict key and function as value
             return func #return func to allow chaining decorators (assign multiple routes to a single function)
         return register
 
-    #TODO add support for <int:var> syntax (only match route if var.isdigit()) and <string:var> (the opposite)
     def match_url(self,url:str) -> Tuple[Optional[str],Dict]:
         for path in self.paths:#loop over all stored paths
             kwargs = {}
@@ -26,7 +31,15 @@ class Flask:
                     if a != b:#check if strings match and are not empty
                         if a == "" or a[0] != "<" and a[-1] != ">":#check if string is a variable or not
                             break#break early and move on to next path
-                        kwargs[a[1:-1]] = b#store variable and name as key word arguments
+                        clean_a = a[1:-1]#remove <> from string
+                        if ":" in a:#check to see if variable has been type cast
+                            var_type,var_name = clean_a.split(":")#split parameter into the type and the name
+                            res = TYPES[var_type](b)#pass variable into converter function for its type
+                            if not res:#break if types don't match
+                                break
+                            kwargs[var_name] = res#store parsed variable and name as key word arguments
+                        else:
+                            kwargs[clean_a] = b#store variable and name as key word arguments
                 else:#runs if loop completes without breaking
                     return path,kwargs#return matched path and variables from url as key word arguments in a tuple
         return None,kwargs#no matching path
@@ -43,9 +56,10 @@ class Flask:
             client,address = server.accept()#recieve client socket and ip address they connected with
             request = client.recv(1024).decode()#recieve request from client and decode byte string
             if len(request) < 1:#break if no data recieved
+                print("shutting down server")
                 break
-            url = request.split(" ")[1]
-            match,kwargs = self.match_url(url)#recieve the matched path and variables associated with requested url
+            url = request.split(" ")[1]#pull requested url from request string
+            match,kwargs = self.match_url(url)#lookup requested url and return matching path and key word arguments associated with it
             if match:#if match was found
                 response = self.paths[match](**kwargs)#pass key word arguments into associated function and get return value
                 byte_str = f"HTTP/1.1 200 OK\nContent-Type: text/html\n\n{response}\n"#pass response into string to send back to client (browser)
@@ -56,8 +70,9 @@ class Flask:
             client.close()#clean up connection to client
 
 def render_template(file:str, **kwargs:Any) -> str:
-    base = os.path.dirname(os.path.realpath(__file__))
-    with open(os.path.join(base,f"templates/{file}"),"r",encoding="utf-8") as f:
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(BASE_DIR,f"templates/{file}"),"r",encoding="utf-8") as f:
         parsed_html = f.read()
-        #TODO parse html file and perform any template logic required, returning a new html str to be sent to the client
+        #TODO parse html file and look for {{}} and compare strings to keys in kwargs, replacing the {{}} with any values for matching keys
+        #TODO parse html file and perform any template logic required (if statements and for loops), returning a new processed html str to be sent to the client
         return parsed_html
--- python/fake_flask/server.py
+++ python/fake_flask/server.py
@@ -13,22 +13,13 @@ def dojo():
 def html_test():
     return render_template("index.html")
 
-@app.route("/say/<name>")
+@app.route("/say/<string:name>")
 def say(name):
-    if not name.isdigit():
-        return f"Hi, {name}"
-    return "invalid request"
+    return f"Hi, {name}"
 
-@app.route("/repeat/<times>/<word>")
+@app.route("/repeat/<int:times>/<string:word>")
 def repeat(times,word):
-    if times.isdigit() and not word.isdigit():
-        return word * int(times)
-    return "invalid request"
-
-# @app.route("/<var>")
-# def error(var):
-#     print(var)
-#     return "Sorry! No response. Try again."
+    return word * int(times)
 
 if __name__=="__main__":
     app.run(debug=True)
