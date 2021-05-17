9bd5da864deba468f0d9806dac6058cc5b3564c0
geoisevil <geoisevil@gmail.com>
2021-05-16 21:31:03 -0700

    updated comments

--- python/dog/flask_app/models/schema_model.py
+++ python/dog/flask_app/models/schema_model.py
@@ -9,33 +9,28 @@ class Schema:
         cols = [f'`{col}`' for col in columns]
         vals = [f'%({col})s' for col in columns]
         return cols,vals
-#C
+#-------------------Create---------------------#
     @classmethod
     def create(cls, **data):
         cols,vals = cls.format_data(data.keys())
         query = f"INSERT INTO `{cls.table}` ({', '.join(cols)}) VALUES ({', '.join(vals)})"
         return connectToMySQL(cls.db).query_db(query,data)
-#R
+#-------------------Retrieve-------------------#
     @classmethod
     def retrieve(cls, **data):#if nothing is passed in select all, otherwise filters by whatever keyword arguments are passed in
         cols,vals = cls.format_data(data.keys())
         query = f"SELECT * FROM `{cls.table}` {'WHERE '+' AND'.join(f' {col}={val}' for col,val in zip(cols,vals)) if data else ''}"
         return connectToMySQL(cls.db).query_db(query,data)
-#U
+#-------------------Update---------------------#
     @classmethod
     def update(cls, **data):
         cols,vals = cls.format_data(data.keys())
         query = f"UPDATE `{cls.table}` SET {', '.join(f'{col}={val}' for col,val in zip(cols,vals))} WHERE id=%(id)s"
         return connectToMySQL(cls.db).query_db(query,data)
-#D
+#-------------------Delete---------------------#
     @classmethod
     def delete(cls, **data):
         cols,vals = cls.format_data(data.keys())
         query = f"DELETE FROM `{cls.table}` WHERE {' AND '.join(f'{col}={val}' for col,val in zip(cols,vals))}"
         return connectToMySQL(cls.db).query_db(query,data)
-
-    @classmethod
-    def join_to(cls, cls2, **data):
-        cols,vals = cls.format_data(data.keys())
-        query = f"SELECT * FROM `{cls.table}` JOIN `{cls2.table}` ON `{cls2.table}_id`=`{cls2.table}`.id {'WHERE '+' AND'.join(f' {col}={val}' for col,val in zip(cols,vals)) if data else ''}"
-        return connectToMySQL(cls.db).query_db(query,data)
+#----------------------------------------------#
