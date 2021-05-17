fd87b7cc0b642ac95732eed8ab19a814cb7bd377
geoisevil <geoisevil@gmail.com>
2021-05-17 10:31:16 -0700

    removed django ninja gold

--- python/ninja_gold_modular/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('reset', views.reset),#reset the game
-    path('process', views.process_money),# post request version of process money
-    path('<location>', views.process_money),# get request version of process money
-]
