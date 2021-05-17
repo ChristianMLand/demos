eed2ba041351712d0309b125e48ad0f0d5d585ef
geoisevil <geoisevil@gmail.com>
2021-05-16 17:48:37 -0700

    refactoring

--- python/dog/flask_app/models/schema_model.py
+++ python/dog/flask_app/models/schema_model.py
@@ -5,31 +5,31 @@ class Schema:
         self.id = row_id
 
     @staticmethod
-    def format_data(**data):
-        cols = [f'`{col}`' for col in data.keys()]
-        vals = [f'%({col})s' for col in data.keys()]
+    def format_data(columns):
+        cols = [f'`{col}`' for col in columns]
+        vals = [f'%({col})s' for col in columns]
         return cols,vals
 #C
     @classmethod
     def create(cls, **data):
-        cols,vals = cls.format_data(**data)
-        query = f'INSERT INTO `{cls.table}` ({", ".join(cols)}) VALUES ({", ".join(vals)})'
+        cols,vals = cls.format_data(data.keys())
+        query = f"INSERT INTO `{cls.table}` ({', '.join(cols)}) VALUES ({', '.join(vals)})"
         return connectToMySQL(cls.db).query_db(query,data)
 #R
     @classmethod
-    def retrieve(cls,**data):
-        cols,vals = cls.format_data(**data)
-        query = f"SELECT * FROM `{cls.table}` {'WHERE' if data else ''} {' AND '.join(f'{col}={val}' for col,val in zip(cols,vals))}"
+    def retrieve(cls, **data):#if nothing is passed in select all, otherwise filters by whatever keyword arguments are passed in
+        cols,vals = cls.format_data(data.keys())
+        query = f"SELECT * FROM `{cls.table}` {'WHERE '+' AND'.join(f' {col}={val}' for col,val in zip(cols,vals)) if data else ''}"
         return connectToMySQL(cls.db).query_db(query,data)
 #U
     @classmethod
     def update(cls, **data):
-        cols,vals = cls.format_data(**data)
+        cols,vals = cls.format_data(data.keys())
         query = f"UPDATE `{cls.table}` SET {', '.join(f'{col}={val}' for col,val in zip(cols,vals))} WHERE id=%(id)s"
         return connectToMySQL(cls.db).query_db(query,data)
 #D
     @classmethod
-    def delete(cls,**data):
-        cols,vals = cls.format_data(**data)
+    def delete(cls, **data):
+        cols,vals = cls.format_data(data.keys())
         query = f"DELETE FROM `{cls.table}` WHERE {' AND '.join(f'{col}={val}' for col,val in zip(cols,vals))}"
         return connectToMySQL(cls.db).query_db(query,data)
